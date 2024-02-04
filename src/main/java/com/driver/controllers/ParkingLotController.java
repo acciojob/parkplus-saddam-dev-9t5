f4b12b9c43bb42f5e06ee65feae4b659bcad0953
saddam-dev-9t5 <saddam.dev.9t5@gmail.com>
2024-02-04 12:32:32 +0530

    Partial submitted by Saddam

--- src/main/java/com/driver/controllers/ParkingLotController.java
+++ src/main/java/com/driver/controllers/ParkingLotController.java
@@ -43,7 +43,7 @@ public class ParkingLotController {
     @PutMapping("/{parkingLotId}/spot/{spotId}/update")
     public ResponseEntity<Spot> updateSpot(@PathVariable int parkingLotId, @PathVariable int spotId, @RequestParam int pricePerHour) {
         //update the details of a spot
-        Spot updatedSpot = parkingLotService.updateSpot(parkingLotId, spotId, pricePerHour)
+        Spot updatedSpot = parkingLotService.updateSpot(parkingLotId, spotId, pricePerHour);
         return new ResponseEntity<>(updatedSpot, HttpStatus.OK);
     }
 
